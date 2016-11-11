3c27aa1abb5b9374f20666dfeee4b44031062e04
Angie Crellin <angiecrellin@gmail.com>
2016-11-11 00:01:03 +0000

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
 
+var ReactDOM = require('react-dom');
+
+var Flashcard = require('./components/Flashcard');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<Flashcard />, document.getElementById('app'));
+});
---
+++ test/test-index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
+var should = require('chai').should();
+
+var Flashcard = require('../js/components/Flashcard');
+
+
+describe('Flashcard component', function() {
+    it('Renders',  function() {
+        var renderer = TestUtils.createRenderer();
+        renderer.render(<Flashcard />);
+        
+      
+      
+        
+    });
+});
